aadcdd73b861878b0ad466124d9ef211f300d076
Sam Absalom <sabsalom93@gmail.com>
2021-11-19 19:54:29 +0000

    Update unifi_wifi.sh

--- unifi_wifi.sh
+++ unifi_wifi.sh
@@ -4,7 +4,7 @@ unifi_username=USERNAME
 unifi_password='PASSWORD'
 unifi_controller=https://EXAMPLE.COM:8443
 wifi_id=YOUR_WIFI_ID
-site_id=default
+site=default
 cookie=/tmp/cookie
 
 curl_cmd="curl -s -S --cookie ${cookie} --cookie-jar ${cookie} --insecure "
@@ -23,19 +23,19 @@ unifi_logout() {
 enable_wifi() {
  # enables wifi network
  # Mute response by adding > /dev/null
- ${curl_cmd} "$unifi_controller"'/api/s/$site_id/rest/wlanconf/'"$wifi_id" -X PUT --data-binary '{"_id":"'"$site_id"'","enabled":true}' --compressed > /dev/null
+ ${curl_cmd} "$unifi_controller"'/api/s/'$site'/rest/wlanconf/'"$wifi_id" -X PUT --data-binary '{"_id":"'"$site_id"'","enabled":true}' --compressed > /dev/null
 }
 
 disable_wifi() {
  # enables guest wifi network
  # Mute response by adding > /dev/null
- ${curl_cmd} "$unifi_controller"'/api/s/$site_id/rest/wlanconf/'"$wifi_id" -X PUT --data-binary '{"_id":"'"$site_id"'","enabled":false}' --compressed > /dev/null
+ ${curl_cmd} "$unifi_controller"'/api/s/'$site'/rest/wlanconf/'"$wifi_id" -X PUT --data-binary '{"_id":"'"$site_id"'","enabled":false}' --compressed > /dev/null
 }
 
 check_status() {
  # checks wifi network status
  # Mute response by adding > /dev/null
- response=$(${curl_cmd} "$unifi_controller"'/api/s/$site_id/rest/wlanconf/'"$wifi_id" --compressed)
+ response=$(${curl_cmd} "$unifi_controller"'/api/s/'$site'/rest/wlanconf/'"$wifi_id" --compressed)
  status=$(echo $response | jq ".data[0].enabled")
  if [ "$status" == "true" ]; then
  exit 0
