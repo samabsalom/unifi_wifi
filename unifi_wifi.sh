372c5c7a2c35518dc832c83c73492ba9e1e9148e
Sam Absalom <sabsalom93@gmail.com>
2021-11-19 20:22:00 +0000

    Update unifi_wifi.sh

--- unifi_wifi.sh
+++ unifi_wifi.sh
@@ -38,8 +38,10 @@ check_status() {
  response=$(${curl_cmd} "$unifi_controller"'/api/s/'$site'/rest/wlanconf/'"$wifi_id" --compressed)
  status=$(echo $response | jq ".data[0].enabled")
  if [ "$status" == "true" ]; then
+ echo "Wifi up"
  exit 0
  elif [ "$status" == "false" ]; then
+ echo "Wifi down"
  exit 1
  else
  echo exit -1
@@ -48,11 +50,15 @@ check_status() {
 
 unifi_login
 if [ "$1" == "enable" ]; then
+ check_status
  echo "Enabling WiFi."
  enable_wifi
+ check_status
 elif [ "$1" == "disable" ]; then
+ check_status
  echo "Disabling WiFi."
  disable_wifi
+ check_status
 elif [ "$1" == "status" ]; then
  check_status
 else
